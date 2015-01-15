6786aee9bd6254678d5971395358a986ad1c9946
Arjun Mehta <arjunmeht@gmail.com>
2015-01-14 17:17:56 -0700

    proper startup and exit escape codes

--- test/test.js
+++ test/test.js
@@ -1,3 +1,26 @@
+var keypress = require('keypress');
+
+
+process.stdout.write("\033[?1049h\033[H");
+
+if(process.stdin.isTTY){
+
+  keypress(process.stdin);  
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', function (ch, key) {
+
+    if (key && ((key.ctrl && key.name == 'c') || key.name == 'q') ) {
+      process.stdin.pause();
+      process.exit(0);
+    }
+  });
+}
+
+process.on('exit', function(){
+	process.stdout.write("\033[?1049l");
+});
+
 var columns = require('../main').create({
     margin: {
         top: 0,
@@ -10,7 +33,7 @@ var columns = require('../main').create({
 columns.addColumn("A1");
 
 columns.addColumn("A2", {
-    width: "25%"
+    width: "50%"
 });
 
 columns.addColumn("A3", {
@@ -24,5 +47,14 @@ columns.addColumn("A5");
 
 
 setInterval(function(){
-	columns.column("A2").write("Hello\n");
+	columns.column("A2").write("Hello\n" + new Date().getTime());
 }, 500);
+
+
+setInterval(function(){
+	columns.column("A3").write("How are you Today???" + columns.column("A3").width + new Date().getTime() +  "\n");
+}, 1000);
+
+setInterval(function(){
+	columns.column("A5").write("ZZZZzzZZ" + columns.column("A5").width + new Date().getTime() +  "\n");
+}, 100);
