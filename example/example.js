c6ed773101a4919868363dd38250d33f3b7ecac3
Arjun Mehta <arjunmeht@gmail.com>
2015-01-15 14:03:29 -0700

    adding tests

---
+++ example/example.js
@@ -0,0 +1,72 @@
+var keypress = require('keypress');
+
+if(process.stdin.isTTY){
+
+  keypress(process.stdin);  
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', function (ch, key) {
+
+    if (key && ((key.ctrl && key.name == 'c') || key.name == 'q') ) {
+      process.stdin.pause();
+      process.exit(0);
+    }
+  });
+}
+
+var columns = require('../main').create({
+    margin: {
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0
+    },
+    separator: ' | ',
+    header_separator: false
+});
+
+columns.addColumn("A1");
+
+columns.addColumn("A2", {
+    width: "50%"
+});
+
+columns.addColumn("A3", {
+    width: 40,
+    header: "testing"
+});
+
+columns.addColumn("A4");
+
+columns.addColumn("A5");
+
+
+setInterval(function(){
+	columns.column("A1").write(randomTruncate("AAAAAAAA" + new Date().getTime() + "AAAAAAAAAAAAAAABBBBSBBSBBBSAAAA"));
+}, 550);
+
+setInterval(function(){
+	columns.column("A2").write(randomTruncate("Hello0000\n" + new Date().getTime() + "JJJJJJJJJJJJJJJAJJAGHAKJGAJHGAJAHGJAHGAJHGAJHGAJHGAJHAGJAHGJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ"));
+}, 500);
+
+
+setInterval(function(){
+	columns.column("A3").write(randomTruncate("\033[32mCCCCCCCCCCCC\n" + new Date().getTime() + "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"));
+}, 500);
+
+
+setInterval(function(){
+	columns.column("A4").write(randomTruncate("How are you Today???\n" + columns.column("A3").width + new Date().getTime() +  "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK\n"));
+}, 1000);
+
+setInterval(function(){
+	columns.column("A5").write(randomTruncate("\033[2KDDDDDDDDDDDDDDDDDD\nDDDDD" + columns.column("A5").width + new Date().getTime() +  "\n"));
+}, 100);
+
+// setInterval(function(){
+// 	columns.column("A1").write(randomTruncate("\033[32mZZZZzzAAAAAAAA"));
+// }, 750);
+
+function randomTruncate(line){
+	return line.substring(0, 6 + Math.random()*50);
+}
